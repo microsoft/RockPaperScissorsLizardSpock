8d4e07ded3fc3604528e180f0c6569082d48ae0f
jldeen <jedeen@outlook.com>
2019-11-14 10:38:44 -0800

    make unattended

--- Deploy/powershell/Setup-Dev-Spaces.ps1
+++ Deploy/powershell/Setup-Dev-Spaces.ps1
@@ -4,7 +4,20 @@ Param(
     [parameter(Mandatory=$true)][string]$aksName
 )
 
+$azdsVersion=$(azds --version)
+
+Write-Host "Your Azure DevSpaces client version is: $azdsVersion"
+Write-Host "Updating Azure DevSpaces client version to latest."
+
+# Update Dev Spaces Client
+wget https://aka.ms/azds-linux-unattended
+sudo sh ./azds-linux-unattended
+
 # Create Dev Spaces controller with some child spaces
-az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace -y
-az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace/scott -y
-az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace/jessica -y
+azds use -g $resourceGroup -n $aksName --space $rootSpace -y
+azds use -g $resourceGroup -n $aksName --space $rootSpace/abel -y
+azds use -g $resourceGroup -n $aksName --space $rootSpace/jessica -y
+
+# az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace -y
+# az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace/abel -y
+# az aks use-dev-spaces -g $resourceGroup -n $aksName --space $rootSpace/jessica -y
