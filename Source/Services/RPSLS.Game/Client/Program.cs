76b1e3739c4eefeb5a4eb9949bbf44a1d4b38bc3
Borja García Rodríguez <bgarcia@plainconcepts.com>
2020-12-03 12:51:12 +0100

    initialize google analitycs for all pages

--- Source/Services/RPSLS.Game/Client/Program.cs
+++ Source/Services/RPSLS.Game/Client/Program.cs
@@ -1,16 +1,18 @@
+using Blazor.Analytics;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RPSLS.Game.Client.Auth;
+using RPSLS.Game.Client.Clients;
+using RPSLS.Game.Client.Extensions;
+using RPSLS.Game.Client.Services;
+using RPSLS.Game.Shared.Config;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Authorization;
-using RPSLS.Game.Client.Auth;
-using RPSLS.Game.Shared.Config;
-using Microsoft.Extensions.Configuration;
-using RPSLS.Game.Client.Clients;
-using RPSLS.Game.Client.Services;
-using Blazor.Analytics;
-using Microsoft.JSInterop;
 
 namespace RPSLS.Game.Client
 {
@@ -31,9 +33,14 @@ namespace RPSLS.Game.Client
             using var stream = await response.Content.ReadAsStreamAsync();
             builder.Configuration.AddJsonStream(stream);
 
+            builder.Services.AddSingleton(services =>
+            {
+                var httpHandler = new GrpcWebHandler(new HttpClientHandler());
+                return GrpcChannel.ForAddress(builder.HostEnvironment.BaseAddress, new GrpcChannelOptions { HttpHandler = httpHandler });
+            });
+
             builder.Services.AddSingleton(_ => builder.Configuration.GetSection(nameof(GameSettingsDto)).Get<GameSettingsDto>());
             builder.Services.AddSingleton(_ => builder.Configuration.GetSection(nameof(GameManagerSettings)).Get<GameManagerSettings>());
-            builder.Services.AddSingleton(_ => builder.Configuration.GetSection(nameof(GoogleAnalyticsSettings)).Get<GoogleAnalyticsSettings>());
             builder.Services.AddSingleton(_ => builder.Configuration.GetSection(nameof(TwitterSettings)).Get<TwitterSettings>());
             builder.Services.AddSingleton(_ => builder.Configuration.GetSection(nameof(MultiplayerSettings)).Get<MultiplayerSettings>());
 
@@ -42,7 +49,8 @@ namespace RPSLS.Game.Client
             builder.Services.AddScoped<IBotGameService, BotGameService>();
             //services.AddScoped<IMultiplayerGameService, MultiplayerGameService>();
 
-            builder.Services.AddGoogleAnalytics();
+            var googleAnalyticsSettings = builder.Configuration.GetSection(nameof(GoogleAnalyticsSettings)).Get<GoogleAnalyticsSettings>();
+            builder.Services.AddGoogleAnalyticsAndInitialize(googleAnalyticsSettings.GoogleAnalytics);
 
             await builder.Build().RunAsync();
         }
