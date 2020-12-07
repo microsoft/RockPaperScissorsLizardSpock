3b7199deee560de0d5d4dc157bb9b15a4a40d1e2
Borja García Rodríguez <bgarcia@plainconcepts.com>
2020-12-07 15:16:28 +0100

    add docker support

--- Source/Services/RPSLS.Game/Server/Startup.cs
+++ Source/Services/RPSLS.Game/Server/Startup.cs
@@ -80,12 +80,9 @@ namespace RPSLS.Game.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseAuthentication();
-            app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
